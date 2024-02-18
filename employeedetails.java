d61ff75270d233360aa534d5593d1c1905c98298
vinaykumar <vinuvinay9505@gmail.com>
2024-02-18 16:07:02 +0530

    commit1 in employeedetails constructors

---
+++ employeedetails.java
@@ -0,0 +1,21 @@
+public class employeedetails {
+    public static void main(String[] args) {
+        employee data = new employee("vinay",1,"manager");
+          System.out.println(data);
+
+          employee sss = new employee();
+          sss.name = "Srisathyasai";
+          sss.empid = 0123;
+          sss.dept = "god";
+          System.out.println(sss);
+
+          employee hdp = new employee();
+          hdp.name = "Hindupur";
+          hdp.empid = 515201;
+          hdp.dept = "Muncipal corporation";
+          System.out.println(hdp);
+
+         
+    }
+    
+}
